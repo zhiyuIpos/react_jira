1c880be735dd6856eb32949654b4fc6ade551b55
zhiyuIpos <zhiyu-@outlook.com>
2021-04-14 20:51:06 +0800

    git commit -m "fix"

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React12
+          Learn React12 131
         </a>
       </header>
     </div>
